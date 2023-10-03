5ffaa0fe8a888b39c27c677931179e87747ee298
CodeF0x <contact@codef0x.dev>
2023-10-04 00:24:52 +0200

    chore: formatting

--- src/schema.rs
+++ src/schema.rs
@@ -29,8 +29,4 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(
-    sessions,
-    tickets,
-    users,
-);
+diesel::allow_tables_to_appear_in_same_query!(sessions, tickets, users,);
